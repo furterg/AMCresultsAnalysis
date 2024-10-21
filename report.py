1b33c89bd1471d72399b6e6838476d3238582530
furterg <gregory.furter@gmail.com>
2024-10-21 23:16:38 +0200

    ``` fix(report): format integer values for examinees and questions (master)
    Ensure that the 'Number of examinees' and 'Number of questions'
    are displayed as integers without decimal points in the report.
    This improves the clarity of the report by presenting these
    values in a more readable format, enhancing the overall
    user experience.
    ```

--- report.py
+++ report.py
@@ -280,7 +280,11 @@ def generate_pdf_report(params: dict):
     pdf.set_font('Helvetica', '', 12)
     for key in ['Number of examinees', 'Number of questions', 'Maximum possible mark',
                 'Minimum achieved mark', 'Maximum achieved mark']:
-        text = str(stats.loc[key]['Value'])
+        # if key in ['Number of examinees', 'Number of questions'], make it integer with no decimal
+        if key in ['Number of examinees', 'Number of questions']:
+            text = str(round(stats.loc[key]['Value']))
+        else:
+            text = str(stats.loc[key]['Value'])
         pdf.cell(w=pw / 5, h=ch, txt=text, ln=0, align='C', border='LBR')
     pdf.ln(ch)
     # Second row of data
